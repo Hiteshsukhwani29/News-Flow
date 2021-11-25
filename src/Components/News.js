0bf8c8944e5df2541f5fa17dec0ec6f4dc6a55fc
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2021-11-25 23:29:38 +0530

    add loading spinner

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItems from './NewsItems'
+import Spinner from './Spinner'
 
 export default class News extends Component {
 
@@ -7,26 +8,71 @@ export default class News extends Component {
         super();
         this.state = {
             articles: [],
-            loading: false
+            loading: true,
+            page:1
         }
     }
 
     async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=aea63673afb948878c1fa9ec38ab82d9";
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=aea63673afb948878c1fa9ec38ab82d9&pagesize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parseddata = await data.json();
+        this.setState({articles: parseddata.articles, totalresults: parseddata.totalResults});
+    }
+
+    handlePrevclick = async ()=>{
+
+
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=aea63673afb948878c1fa9ec38ab82d9&page=${this.state.page - 1}&pagesize=${this.props.pageSize}`;
+        this.setState({ loading: true });
         let data = await fetch(url);
         let parseddata = await data.json();
         this.setState({articles: parseddata.articles});
+
+        this.setState({
+            page: this.state.page-1,
+            loading: false,
+        })
+        console.log(this.page);
+
+    }
+
+    handleNextclick = async ()=>{
+
+        if(this.state.page + 1 > Math.ceil(this.state.totalresults/this.props.pageSize)){
+
+        }
+        else{
+
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=aea63673afb948878c1fa9ec38ab82d9&page=${this.state.page + 1}&pagesize=${this.props.pageSize}`;
+        this.setState({ loading: true });
+        let data = await fetch(url);
+        let parseddata = await data.json();
+        this.setState({articles: parseddata.articles});
+
+
+        this.setState({
+            page: this.state.page+1,
+            loading: false,
+        })
+    }
+
     }
 
     render() {
         return (
-            <div className="row offset-md-1 mt-3">
-                <div className="row">
+            <div className="container mt-3 mb-3">
+                {this.state.loading && <Spinner/>}
+                <div className="row" style={{margin:"0px"}}>
                     {this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
-                            <NewsItems title={element.title?element.title.slice(0, 45):"" + "..."} desc={element.description?element.description.slice(0, 88):"" + "..."} imgurl={element.urlToImage?element.urlToImage:"https://www.dreamhost.com/blog/wp-content/uploads/2021/03/5cb3549d-46d2-44ec-8e32-3598ad4600f8_how-to-fix-wordpress-404-error_opt-750x498.jpg"} newsUrl={element.url} />
+                        return <div className="col-md-3" key={element.url}>
+                            <NewsItems title={element.title?element.title.slice(0, 30):"" + "..."} desc={element.description?element.description.slice(0, 88):"" + "..."} imgurl={element.urlToImage?element.urlToImage:"https://www.dreamhost.com/blog/wp-content/uploads/2021/03/5cb3549d-46d2-44ec-8e32-3598ad4600f8_how-to-fix-wordpress-404-error_opt-750x498.jpg"} newsUrl={element.url} />
                         </div>
                     })}
+                    <div className="container d-flex justify-content-between mt-5">
+                        <button disabled={this.state.page<=1?true:false} type="button" className="btn btn-primary" onClick={this.handlePrevclick}>&larr; Previous</button>
+                        <button type="button" className="btn btn-primary" onClick={this.handleNextclick}>Next &rarr;</button>
+                    </div>
                 </div>
             </div>
         )
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -5,12 +5,12 @@ export default class NewsItems extends Component {
         let {title,desc,imgurl,newsUrl} = this.props;
         return (
             <div>
-                <div className="card" style={{width: "18rem"}}>
-                    <img src={imgurl} className="card-img-top" alt="..."/>
+                <div className="card mb-3" style={{height: "380px"}}>
+                    <img src={imgurl} className="card-img-top" alt="..." style={{height: "150px"}}/>
                     <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{desc}</p>
-                    <a href={newsUrl} traget="_blank" className="btn btn-primary">Read More</a>
+                    <a href={newsUrl} traget="_blank" className="btn btn-outline-primary">Read More</a>
                     </div>
                 </div>
             </div>
